6bb85bc20376577c95df65b3f40a2e10dfdef811
Soran Shim <ssl3737@hotmail.com>
2018-04-07 15:10:27 -0700

    commit burger builder project

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = (props) => {
+    return (
+        <div className={classes.BuildControls}>
+            {controls.map(ctrl => (
+                <BuildControl 
+                    key={ctrl.label} 
+                    label={ctrl.label}
+                    type={ctrl.type}
+                    added={() => props.ingredientsAdded(ctrl.type)}
+                    removed={() => props.ingredientsRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]}/>
+            ))}
+        </div>
+    );
+};
+
+export default buildControls;
